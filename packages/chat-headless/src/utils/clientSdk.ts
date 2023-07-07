9ec37570d8a1e780ffe8519d6feeb90866073792
Yen Truong <ytruong@yext.com>
2023-07-07 10:03:33 -0400

    add docs, bump version

--- packages/chat-headless/src/utils/clientSdk.ts
+++ packages/chat-headless/src/utils/clientSdk.ts
@@ -4,6 +4,12 @@ import packageJson from "../../package.json";
 const { version } = packageJson;
 const coreVersion = packageJson.dependencies["@yext/chat-core"];
 
+/**
+ * Creates new mapping for clientSdk by merging data from param additionalClientSdk
+ * with chat-headless's version and chat-core's version extracted from package.json
+ *
+ * @internal
+ */
 export function getClientSdk(
   additionalClientSdk?: ChatEventPayLoad["clientSdk"]
 ): ChatEventPayLoad["clientSdk"] {
