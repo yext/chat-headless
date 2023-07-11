6d13b079ea05ea9754fc6443b996c00daa960834
nbramblett <nbramblett@yext.com>
2023-07-11 15:35:37 -0400

    comments

--- packages/chat-headless-react/src/ChatHeadlessProvider.tsx
+++ packages/chat-headless-react/src/ChatHeadlessProvider.tsx
@@ -25,6 +25,9 @@ export function ChatHeadlessProvider(
   props: ChatHeadlessProviderProps
 ): JSX.Element {
   const { children, config } = props;
+  // deferLoad is used with sessionStorage so that the children won't be
+  // immediately rendered and trigger the "load initial message" flow before
+  // the state can be loaded from session.
   const [deferLoad, setDeferLoad] = useState(config.saveToSessionStorage);
 
   const headless = useMemo(() => {
@@ -32,6 +35,10 @@ export function ChatHeadlessProvider(
     const headless = new ChatHeadless(updateClientSdk(configWithoutSession));
     return headless;
   }, [config]);
+
+  // sessionStorage is overridden here so that it is compatible with server-
+  // side rendering, which cannot have browser api calls like session storage
+  // outside of hooks.
   useEffect(() => {
     if (!config.saveToSessionStorage || !headless) {
       return;
